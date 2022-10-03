0d20afa6069589691abe9f7ba5867199cfeadcc5
Adrian <acalerobarrero@gmail.com>
2022-10-03 11:02:27 +0200

    Add component myProject in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.css';
 import { Header } from '../../components/home/Header';
 import About from '../../components/home/About';
+import MyProjects from '../../components/home/MyProjects';
 
 export function Home() {
   return (
     <>
       <Header />
       <About />
+      <MyProjects />
     </>
   );
 }
